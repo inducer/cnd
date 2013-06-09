11681cfe23e4a8ef9c3ab691c6b362a4b9161271
Andreas Kloeckner <inform@tiker.net>
2013-06-09 14:22:30 -0400

    Switch to setuptools

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 # -*- coding: latin1 -*-
 
 def main():
-    import distribute_setup
-    distribute_setup.use_setuptools()
+    import ez_setup
+    ez_setup.use_setuptools()
 
     from setuptools import setup
 
@@ -36,7 +36,7 @@ def main():
          )
 
 if __name__ == "__main__":
-    import distribute_setup
-    distribute_setup.use_setuptools()
+    import ez_setup
+    ez_setup.use_setuptools()
 
     main()
